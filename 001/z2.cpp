adb7bd104fd1f4a17047d2390d16ceac20ea1924
Cimlah <28655852+Cimlah@users.noreply.github.com>
2021-09-27 17:52:25 +0200

    Napisałem kod do drugiego zadania (projekt '001')

---
+++ 001/z2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string text;
+    int czy_cyfry=0;
+
+    cout << "Wprowadź jakiś tekst: ";
+    getline(cin, text);
+
+    for(int i=0; i<text.length(); i++) {
+        if(text[i]>='1' && text[i]<='9') czy_cyfry++;
+    }
+
+    if(czy_cyfry>0) cout << "W tekście występują cyfry";
+    else cout << "W tekście nie ma żadnych cyfr";
+
+    cout << endl;
+
+    return(0);
+}
