c4650e5eddb0ba1816d9a4c59cc7ee34341b0fbf
Cimlah <28655852+Cimlah@users.noreply.github.com>
2021-09-27 18:16:53 +0200

    Stworzyłem plik .gitignore... aby niektóre pliki nie dodawały się do repozytorium

---
+++ 001/z3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string text;
+    int licznik_malych_liter=0, licznik_wielkich_liter=0;
+
+    cout << "Wprowadź jakiś tekst: ";
+    getline(cin, text);
+}
