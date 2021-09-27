1920fe650b3109ba69e2ee89b2c388ba55bd39ef
Cimlah <28655852+Cimlah@users.noreply.github.com>
2021-09-27 18:24:50 +0200

    Napisałem kod do trzeciego zadania (projekt '001')

--- 001/z3.cpp
+++ 001/z3.cpp
@@ -8,4 +8,12 @@ int main() {
 
     cout << "Wprowadź jakiś tekst: ";
     getline(cin, text);
+
+    for(int i=0; i<text.length(); i++) {
+        if(text[i]>='A' && text[i]<='Z') licznik_wielkich_liter++;
+            else if(text[i]>='a' && text[i]<='z') licznik_malych_liter++;
+    }
+
+    cout << "Ilość wielkich liter: " << licznik_wielkich_liter << endl;
+    cout << "Ilość małych liter: " << licznik_malych_liter << endl;
 }
