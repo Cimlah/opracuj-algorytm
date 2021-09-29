1d618d2b331c311910024a88ae1138cd59da500a
Cimlah <28655852+Cimlah@users.noreply.github.com>
2021-09-29 17:21:19 +0200

    Skończyłem pisać kod do zadania siódmego (projekt '001')

--- 001/z7.cpp
+++ 001/z7.cpp
@@ -3,8 +3,28 @@
 using namespace std;
 
 int main() {
-    string text;
+    string text, slowo="", wspak="";
 
     cout << "Wprowadź jakiś tekst: ";
     getline(cin, text);
+    text+=' ';
+    cout << "Palindromy: ";
+
+    for(int i=0; i<text.length(); i++) {
+        if(text[i]==' ') {
+            for(int j=slowo.length()-1; j>=0; j--) {
+                wspak+=slowo[j];
+            }
+
+            if(slowo==wspak) cout << slowo << "; ";
+
+            slowo="";
+            wspak="";
+        }
+        else slowo+=text[i];
+    }
+
+    cout << endl;
+
+    return(0);
 }
